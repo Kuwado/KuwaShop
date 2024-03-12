0ff7176e7ce09dec28c8f8b1981e66c891f300f9
“Kuwado” <viethoan557723@gmail.com>
2024-03-13 03:22:05 +0700

    home

--- Js/slide.js
+++ Js/slide.js
@@ -7,13 +7,22 @@ const textPosition = document.querySelectorAll(".overlay")
 const textContainer = document.querySelector(".over")
 let imgNumber = imgPosition.length
 let index = 0
+let interval; // Biến lưu trữ interval
+
+// Hàm để bắt đầu hoặc restart interval
+function startInterval() {
+    clearInterval(interval); // Xóa interval hiện tại (nếu có)
+    //interval = setInterval(imgSlide, 5000); // Khởi tạo interval mới
+}
 
 imgPosition.forEach(function (image, i) {
+    startInterval(); // Bắt đầu hoặc restart interval
     image.style.left = i * 120 + "%"                            // trải dài các ảnh sang trái
     textPosition[i].style.left = 50 + i * 120 + "%"; // Đặt vị trí của phần overlay
     dotItem[i].addEventListener("click", function () {
         slider(i)
         index = i
+        startInterval(); // Bắt đầu hoặc restart interval
     })
 })
 
@@ -32,10 +41,12 @@ prevBtn.addEventListener("click", function(){
         index = imgNumber - 1
     }
     slider(index)
+    startInterval(); // Bắt đầu hoặc restart interval
 })
 
 nextBtn.addEventListener("click", function(){
     imgSlide()
+    startInterval(); // Bắt đầu hoặc restart interval
 })
 
 // hiện thị ảnh với vị trí tương ứng đồng thời clear hết active và set active mới cho dot hiện tại
@@ -48,4 +59,5 @@ function slider(index) {
     dotItem[index].classList.add("active")
 
 }
-//setInterval(imgSlide, 7000)
+
+//setInterval(imgSlide, 5000)
