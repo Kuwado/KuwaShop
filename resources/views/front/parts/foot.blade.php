e3253cb0cb0a4c72b56b819430589f98e4b778dd
“Kuwado” <viethoan557723@gmail.com>
2024-05-08 11:59:30 +0700

    start connecting to user view

---
+++ resources/views/front/parts/foot.blade.php
@@ -0,0 +1,10 @@
+
+    <!--Java Script-->
+    <script src="../Js/main.js"></script> <!--Main-->
+    <script src="../Js/slide.js"></script> <!--Slide-->
+    <script src="../Js/product.js"></script> <!--Slide-->
+    <!--End of JS-->
+</body>
+
+
+</html>
